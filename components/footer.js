56d9e45aab0483a99b352446b0ea02b4faf46487
Corey Chapman <corey.chapman@infotrustllc.com>
2024-11-21 16:16:26 -0500

    we cookin baby

---
+++ components/footer.js
@@ -0,0 +1,8 @@
+const footer = `
+<footer class="absolute bottom-0 flex justify-center items-center w-full">
+  <div class="flex space-x-8 py-4">
+    <a href="#" class="text-sm text-gray-500 hover:text-gray-700">About Us</a>
+    <a href="#" class="text-sm text-gray-500 hover:text-gray-700">Contact Us</a>
+    <a href="#" class="text-sm text-gray-500 hover:text-gray-700">Your Privacy Settings</a>
+  </div>
+</footer>`;
